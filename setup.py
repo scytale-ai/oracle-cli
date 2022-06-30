f69644d1f531e21a9e4daadeb3f5e5152a1bdd31
evoosa <eva@scytale.ai>
2022-06-30 13:58:28 +0300

    added requirements

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
+import os
+
 from setuptools import setup
+
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirement_path = os.path.join(lib_folder, 'requirements.txt')
+install_requires = []
+if os.path.isfile(requirement_path):
+    with open(requirement_path) as f:
+        install_requires = f.read().splitlines()
 
 setup(
     name='scytale-oracle',
@@ -7,6 +16,7 @@ setup(
     entry_points={
         'console_scripts': ['scytale-oracle=scytale_oracle.cli.cli:run_cli']
     },
+    install_requires=install_requires,
     url='https://scytale.ai/',
     license='GNU General Public License v3.0',
     author='evoosa & idan91',
